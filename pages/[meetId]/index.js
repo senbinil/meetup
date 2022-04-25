4d1fa24f2730cc9c187ee284144967676c04ac37
senbinil <senoffical111@gmail.com>
2022-04-25 18:57:51 +0530

    fixed fallback

--- pages/[meetId]/index.js
+++ pages/[meetId]/index.js
@@ -26,7 +26,7 @@ export async function getStaticPaths() {
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
   client.close();
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meet) => ({
       params: { meetId: meet._id.toString() },
     })),
